fc6a2b2729fbe47d1c1c90feb2630cd7555f43f9
Joao Pedro <joaopedrovp.17@gmail.com>
2025-05-05 10:26:58 -0300

    update
    Iniciado o desenvolvimento do card principal da página

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 // script.js
 
-const menuToggle = document.querySelector(' .menu-toggle');
-const navMenu = document.querySelector(' .nav-menu');
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const navMenu = document.querySelector(".nav-menu");
 
-menuToggle.addEventListener('click', () =>{
-    navMenu.classList.toggle('show');   
+    menuToggle.addEventListener("click", () => {
+        navMenu.classList.toggle("show");
+    });
+
+    // Fecha o menu ao clicar em um item do menu mobile
+    const mobileLinks = navMenu.querySelectorAll(".mobile-only a");
+    mobileLinks.forEach(link => {
+        link.addEventListener("click", () => {
+            navMenu.classList.remove("show");
+        });
+    });
 });
+
