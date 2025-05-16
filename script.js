1675f7f2acb9ca8a18a56c198b02d8332a4360cd
Joao Pedro <joaopedrovp.17@gmail.com>
2025-05-16 13:16:07 -0300

    Update

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ document.addEventListener("DOMContentLoaded", function () {
     const menuToggle = document.querySelector(".menu-toggle");
     const navMenu = document.querySelector(".nav-menu");
 
-    menuToggle.addEventListener("click", () => {
+    menuToggle.addEventListener("click", (event) => {
+        // Impedir que o clique no menuToggle se propague para o document e feche imediatamente o menu 
+        event.stopPropagation();
         navMenu.classList.toggle("show");
     });
 
@@ -14,6 +16,16 @@ document.addEventListener("DOMContentLoaded", function () {
         link.addEventListener("click", () => {
             navMenu.classList.remove("show");
         });
+    });
+    // Fecha  o menu ao clicar fora
+    document.addEventListener("click", (event) => {
+        // Verifica se o navMenu está visível
+        if (navMenu.classList.contains("show")) {
+            // Verifica se o clique NÃO foi dentro do navMenu E NÃO foi no menuToggle
+            if (!navMenu.contains(event.target) && !menuToggle.contains(event.target)) {
+                navMenu.classList.remove("show");
+            }
+        }
     });
 });
 
